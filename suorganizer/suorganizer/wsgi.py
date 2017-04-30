2e56ea48a4ded15142ea87349b073b46e99ff274
ChrisH <geochris35@gmail.com>
2017-04-30 15:08:54 -0500

    finished chapter 3

--- suorganizer/suorganizer/wsgi.py
+++ suorganizer/suorganizer/wsgi.py
@@ -7,19 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
- import os
+import os
 
- from django.core.cache.backends.memcached import \
-    BaseMemcachedCache
- from django.core.wsgi import get_wsgi_application
- from whitenoise.django import DjangoWhiteNoise
+from django.core.cache.backends.memcached import \
+	BaseMemcachedCache
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
- os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suorganizer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suorganizer.settings")
 
- BaseMemcachedCache.close = lambda self, **kwargs: None
- 
- application = get_wsgi_application()
- application = DjangoWhiteNoise(application)
+BaseMemcachedCache.close = lambda self, **kwargs: None
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 """
 old Code
