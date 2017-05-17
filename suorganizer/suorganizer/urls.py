0d6a6428061f4a3f80141146877efa07cc4c0f7a
ChrisH <geochris35@gmail.com>
2017-05-16 19:37:44 -0500

    removed .pyc

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage
+from organizer.views import homepage, tag_detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', homepage),
+    url(r'^tag/(?P<slug>[\w\-]+)/$',
+        tag_detail,
+        name='organizer_tag_detail'),
 ]
