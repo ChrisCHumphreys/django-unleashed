188aeee2f1b1d859e313578871a46f95c7ea545a
ChrisH <geochris35@gmail.com>
2017-05-22 18:07:54 -0500

    Chapter 6 done

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -1,21 +1,41 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+
 from organizer.models import Startup, Tag
 
 # Create your models here.
 
-class Post(models.Model):
 
+
+
+class Post(models.Model):
     title = models.CharField(max_length = 63)
-    slug = models.SlugField(max_length = 63, help_text = 'A label for a URL config', unique_for_month = 'pub_date', )
+    slug = models.SlugField(
+        max_length = 63,
+        help_text = 'A label for a URL config',
+        unique_for_month = 'pub_date', )
     text = models.TextField()
-    pub_date = models.DateField('date published', auto_now_add = True)
-    tags = models.ManyToManyField(Tag, related_name = 'blog_posts')
-    startups = models.ManyToManyField(Startup, related_name = 'blog_posts')
+    pub_date = models.DateField(
+        'date published',
+        auto_now_add = True)
+    tags = models.ManyToManyField(
+        Tag, related_name = 'blog_posts')
+    startups = models.ManyToManyField(
+        Startup, related_name = 'blog_posts')
 
     def __str__(self):
-        return "{} on {}".format(self.title, self.pub_date.strftime('%Y-%m-%d'))
+        return "{} on {}".format(
+            self.title,
+            self.pub_date.strftime('%Y-%m-%d'))
 
     class Meta:
         verbose_name = 'blog post'
         ordering = ['-pub_date', 'title']
         get_latest_by = 'pub_date'
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog_post_detail',
+            kwargs={'year': self.pub_date.year,
+                    'month': self.pub_date.month,
+                    'slug': self.slug})
--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -1,11 +1,18 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
 
-class Tag(models.Model):
 
-    name = models.CharField(max_length = 31, unique = True)
-    slug = models.SlugField(max_length = 31, unique = True, help_text = 'A label for a URL config')
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length = 31, unique = True)
+    slug = models.SlugField(
+        max_length = 31,
+        unique = True,
+        help_text = 'A label for a URL config')
 
     def __str__(self):
         return self.name.title()
@@ -13,12 +20,21 @@ class Tag(models.Model):
     class Meta:
         ordering = ['name']
 
-class Startup(models.Model):
+    def get_absolute_url(self):
+        return reverse('organizer_tag_detail',
+                       kwargs={'slug': self.slug})
 
-    name = models.CharField(max_length = 31)
-    slug = models.SlugField(unique = True)
+
+class Startup(models.Model):
+    name = models.CharField(
+        max_length = 31, db_index=True)
+    slug = models.SlugField(
+        max_length = 31,
+        unique = True,
+        help_text = 'A label for a url config')
     description = models.TextField()
-    founded_date = models.DateField('date founded')
+    founded_date = models.DateField(
+        'date founded')
     contact = models.EmailField()
     website = models.URLField(max_length = 255)
     tags = models.ManyToManyField(Tag)
@@ -29,6 +45,10 @@ class Startup(models.Model):
     class Meta:
         ordering = ['name']
         get_latest_by = 'founded_date'
+
+    def get_absolute_url(self):
+        return reverse('organizer_startup_detail',
+                       kwargs={'slug': self.slug})
 
 class NewsLink(models.Model):
 
